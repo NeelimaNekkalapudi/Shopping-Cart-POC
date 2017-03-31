4e55de97bb44222fb7a620d5d265de6b03036fa2
sshireesha <Shireesha.Surapaneni3@T-Mobile.com>
2017-03-31 17:12:12 +0530

    ConfigChanges

--- shopping-cart-ui/src/main/resources/static/app/configs.js
+++ shopping-cart-ui/src/main/resources/static/app/configs.js
@@ -1,6 +1,6 @@
 app.constant('PRODUCTS_SERVER_PORT','http://localhost:3000');
 app.constant('ACCESSORIES_SERVER_PORT','http://localhost:3000');
-app.constant('CHECKOUT_SERVER_PORT','http://localhost:3000');
+app.constant('CHECKOUT_SERVER_PORT','http://localhost:8182');
 app.constant('CART_SERVER_PORT','http://localhost:3000');
 app.constant('SERVER_PORT','http://localhost:3000');
 //app.constant('SERVER_PORT','http://172.16.203.103:3000');
