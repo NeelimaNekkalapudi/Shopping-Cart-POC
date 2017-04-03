2ffe7af2106faec0b5ebdc7fb0704c6b760f00d6
kvajramani <Vajramani.Kammari@t-mobile.com>
2017-04-03 16:12:58 +0530

    Checkout Changes

---
+++ checkOut-service-api/src/main/java/com/CheckOut/model/OrderDetails.java
@@ -0,0 +1,19 @@
+package com.CheckOut.model;
+
+/**
+ * Created by kvajramani on 31-03-2017.
+ */
+public class OrderDetails {
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String orderId;
+
+
+}
