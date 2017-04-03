00a9ada38cc751901961f6113fc0c53a37435cbd
sshireesha <Shireesha.Surapaneni3@T-Mobile.com>
2017-04-03 17:23:06 +0530

    CheckoutChanges

--- checkOut-service-api/src/main/java/com/CheckOut/CheckOutApplication.java
+++ checkOut-service-api/src/main/java/com/CheckOut/CheckOutApplication.java
@@ -3,6 +3,7 @@ package com.CheckOut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,8 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
 
-@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 @EnableMongoRepositories("com.CheckOut.repository")
 public class CheckOutApplication {
 
@@ -20,7 +22,7 @@ public class CheckOutApplication {
 		SpringApplication.run(CheckOutApplication.class, args);
 	}
 
-   /* @Component
+   @Component
     public class SimpleCORSFilter implements ContainerResponseFilter {
         @Override
         public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
@@ -29,5 +31,5 @@ public class CheckOutApplication {
             headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
             headers.add("Access-Control-Allow-Headers", "*");
         }
-    }*/
+    }
 }
