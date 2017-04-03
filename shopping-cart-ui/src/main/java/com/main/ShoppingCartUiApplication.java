2e7856600c973396165352986d590d730138c24f
Yalamanda Reddy Rodda <YalamandaReddy.Rodda@T-Mobile.com>
2017-04-03 12:35:33 +0530

    Added Eureka Discovery client

--- shopping-cart-ui/src/main/java/com/main/ShoppingCartUiApplication.java
+++ shopping-cart-ui/src/main/java/com/main/ShoppingCartUiApplication.java
@@ -2,6 +2,7 @@ package com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @Controller
+@EnableDiscoveryClient
 public class ShoppingCartUiApplication {
 
 	public static void main(String[] args) {
