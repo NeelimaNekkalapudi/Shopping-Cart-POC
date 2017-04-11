aa99b21babd0d868a62d887cfb2b1fb7c62f0ac7
sshireesha <Shireesha.Surapaneni3@T-Mobile.com>
2017-04-11 12:50:56 +0530

    CheckoutSpringCloudChanges

--- checkOut-service-api/src/main/java/com/CheckOut/CheckOutApplication.java
+++ checkOut-service-api/src/main/java/com/CheckOut/CheckOutApplication.java
@@ -1,20 +1,10 @@
 package com.CheckOut;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Component;
 
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MultivaluedMap;
-import java.io.IOException;
-
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableMongoRepositories("com.CheckOut.repository")
 public class CheckOutApplication {
 
@@ -22,14 +12,14 @@ public class CheckOutApplication {
 		SpringApplication.run(CheckOutApplication.class, args);
 	}
 
-   @Component
-    public class SimpleCORSFilter implements ContainerResponseFilter {
-        @Override
-        public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-            MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-            headers.add("Access-Control-Allow-Origin", "*");
-            headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-            headers.add("Access-Control-Allow-Headers", "*");
-        }
-    }
+//   @Component
+//    public class SimpleCORSFilter implements ContainerResponseFilter {
+//        @Override
+//        public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+//            MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+//            headers.add("Access-Control-Allow-Origin", "*");
+//            headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+//            headers.add("Access-Control-Allow-Headers", "*");
+//        }
+//    }
 }
