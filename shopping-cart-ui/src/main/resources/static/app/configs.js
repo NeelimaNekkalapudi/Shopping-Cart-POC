06ae76953af6ababf5a0d7c650695d5eb5440952
nneelima <VenkataSuryaNeelima.Nekkalapudi1@T-Mobile.com>
2017-04-11 20:13:16 +0530

    orderhistory

--- shopping-cart-ui/src/main/resources/static/app/configs.js
+++ shopping-cart-ui/src/main/resources/static/app/configs.js
@@ -3,4 +3,5 @@ app.constant('ACCESSORIES_SERVER_PORT','http://localhost:7777');
 app.constant('CHECKOUT_SERVER_PORT','http://localhost:8182');
 app.constant('CART_SERVER_PORT','http://localhost:3000');
 app.constant('SERVER_PORT','http://localhost:3000');
+app.constant('DASHBOARD','http://localhost:3001');
 //app.constant('SERVER_PORT','http://172.16.203.103:3000');
